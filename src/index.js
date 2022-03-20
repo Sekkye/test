472952c8a68de9a6bae5bcf886036ddeab0b7bba
Sekkye <90229738+Sekkye@users.noreply.github.com>
2022-03-20 09:31:41 +0900

    変更加えました

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import "./styles.css";
 
 document.getElementById("app").innerHTML = `
-<h1>下剋上。もう一度！！</h1>
+<h1>下剋上。もう一度！！うおおおおおおお</h1>
 <div>
   We use the same configuration as Parcel to bundle this sandbox, you can find more
   info about Parcel 
